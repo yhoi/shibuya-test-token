602648913d7bdf7d86eb4adec6210daa3b66e41a
yhoi <ddmmmktennis@gmail.com>
2022-04-27 08:21:23 +0900

    完成例

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: "0.8.4",
+  networks:{
+    shibuya: {
+      url:"https://rpc.shibuya.astar.network:8545",
+      chainId:81,
+      accounts:[process.env.PRIVATE_KEY],
+    }
+  }
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const ERC20 = await hre.ethers.getContractFactory("TEST");
+  const erc20 = await ERC20.deploy();
+
+  await erc20.deployed();
+
+  console.log("ERC20 Token deployed to:", erc20.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
